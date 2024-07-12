68e9a0efdbc6e7cf09723cce8e13af18c46f9e66
Andrey <anostanin.2016@gmail.com>
2024-07-12 17:35:35 +0500

    Find max and min value of the list

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ from random import randint
 n=20
 a=[randint(50, 150) for i in range(n)]
 print(a)
+M=a[0]
+for x in a:
+    if x>M:
+        M=x
+nMax=0
+for i in range(1,n):
+    if a[i]>a[nMax]:
+        nMax=i
+print("A[", nMax, "]=",a[nMax])
+for x in a:
+    if x<M:
+        M=x
+nMin=0
+for i in range(1,n):
+    if a[i]<a[nMin]:
+        nMin=i
+print("A[", nMin, "]=",a[nMin])
